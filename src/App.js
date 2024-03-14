959d9af4030697ffdcc37a6c71f865fae08bf5be
golovindmitrii <itgolovindmitrii@gmail.com>
2024-03-14 12:40:34 +0500

    6 commit: add mockapi(backend) + useEffect

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import React from 'react';
 
 export default function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  React.useEffect(() => {
+    fetch('https://65c64bc3e5b94dfca2e1528d.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => setPizzas(json));
+  }, []);
+
   return (
     <>
       <div className="wrapper">
